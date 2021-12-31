7637073adee9619155660fcea776a841b7c68d68
Won-young <dnjsdud2257@gmail.com>
2021-12-31 13:20:32 +0900

    수강 후 연습 init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,3 @@
-import React, { useState } from "react";
-import AddUser from "./Component/User/AddUser";
-import UserList from "./Component/User/UserList";
-
-function App() {
-  const [userList, setUserList] = useState([]);
-
-  const addUserHandler = (uName, uAge) => {
-    setUserList((prevUser) => {
-      return [...prevUser, { name: uName, age: uAge, id: Math.random().toString() }];
-    });
-  };
-
-  return (
-    <div>
-      <AddUser onAddUser={addUserHandler} />
-      <UserList users={userList} />
-    </div>
-  );
-}
+function App() {}
 
 export default App;
